3614f70d34ce889ad5fdc5c78c1d517c875a01a5
horfee <horfee@gmail.com>
2024-09-20 09:50:01 +0200

    utility to generate dockerfile description

---
+++ generate-docker-desc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# generate descriptor for dockerfile
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+dockerFile=$1
+images=($(sed -n -e 's/^FROM\s*\(.*\):\([0-9]*\.[0-9]*.[0-9]*\)\s*[aA][sS]\s\s*\(\w*\)/\3;\(\2\)/p' $dockerFile))
+
+str=$(join_by "/" "${images[@]}")
+str=${str//\//" / "}
+str=${str//;/ }
+
+echo $str > "temp$2.desc"
