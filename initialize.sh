834eb7c8508d7eb52abfbe6160b5bb582ff65d5d
Horfee <horfee@gmail.com>
2024-09-26 10:50:47 +0200

    simplifying initializing process (entitlement key is not systematically required)

--- initialize.sh
+++ initialize.sh
@@ -35,7 +35,7 @@ dockerexists=$?
 if [[ "${dockerexists}" != "0" ]]; then
   # Add Docker's official GPG key:
   sudo apt-get update
-  sudo apt-get install ca-certificates curl
+  sudo apt-get -f install ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -45,10 +45,10 @@ if [[ "${dockerexists}" != "0" ]]; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
   sudo apt-get update
-
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+  sudo apt-get install -f docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  
   # Enable non root docker users
   sudo groupadd docker
   sudo usermod -aG docker $USER
@@ -63,24 +63,63 @@ if [[ "${dockerexists}" != "0" ]]; then
   sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.1
 fi
 
+skopeoexists=$(skopeo -v)
+skopeoexists=$?
 
-if [[ -z "${entitlementkey}" ]]; then
-  echo "================================================="
-  echo "You need now to login against IBM ICR"
-  echo "================================================="
-
-  echo -n "Enter ibm entitlement key : "
-  read entitlementkey
-  echo $entitlementkey
+if [[ "${skopeoexists}" != "0" ]]; then
+  sudo apt-get install -f skopeo
 fi
 
-echo "Trying to login against IBM ICR"
-sudo docker login -u cp -p $entitlementkey cp.icr.io
+  
+if [[ -z "${entitlementkey}" ]]; then   # entitlement key not provided : we must test if already provided ; if not, need to request it from stdin
 
-if [[ $? != 0 ]]; then
-  echo "Credentials seems to be wrong. please verify"
-  exit 1
+  needsentitlementkey=$(sudo skopeo list-tags docker://cp.icr.io/cp/manage/manageadmin)
+  needsentitlementkey=$?
+
+  if [[ "${needsentitlementkey}" == "0" ]]; then
+    echo "Existing credentials seems correct."
+    echo "No need to provide them again."
+    echo ""
+  else
+    echo "================================================="
+    echo "You need now to login against IBM ICR"
+    echo "================================================="
+
+    echo -n "Enter ibm entitlement key : "
+    read entitlementkey
+    echo $entitlementkey
+
+    echo "Trying to login against IBM ICR"
+    sudo docker login -u cp -p $entitlementkey cp.icr.io
+
+    if [[ $? != 0 ]]; then
+      echo "Credentials seems to be wrong. please verify"
+      exit 1
+    fi
+
+    #everything is fine, but as we are running docker login with sudo, the credentials are stored in the root home directory
+    #we need to copy it locally if we want run later docker commands without sudo
+    if [ ! -d "$HOME/.docker/" ]; then
+      mkdir $HOME/.docker
+    fi
+    
+    sudo cp /root/.docker/config.json $HOME/.docker/
+    sudo chown $USER:$USER /$HOME/.docker/config.json
+
+  fi
+
+  
+else    # entitlement key provided : we need to override if one already exists
+  echo "Trying to login against IBM ICR"
+  sudo docker login -u cp -p $entitlementkey cp.icr.io
+
+  if [[ $? != 0 ]]; then
+    echo "Credentials seems to be wrong. please verify"
+    exit 1
+  fi
+
 fi
+
 
 user_choice=
 deployments=($(ls -1 *.desc))
@@ -97,7 +136,7 @@ do
     ((i++))
   done
 
-  echo -n "Your choice : "
+  echo -n "Your choice (Control+C to quit): "
   read user_choice
   if [[ "${user_choice}" -lt "0" ]] || [[ "${user_choice}" -gt "${nbdeployments}" ]] || [[ $((user_choice)) != $user_choice ]]; then
     user_choice=
